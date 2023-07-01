a64e1fa5e2f23b7a8b525651f577025ca23e93af
ahmadisyraf <isyrafmagic@gmail.com>
2023-07-01 15:59:50 +0800

    Add works

--- pages/works.js
+++ pages/works.js
@@ -1,11 +1,41 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Image, useMediaQuery, Heading, Text } from "@chakra-ui/react"
 
+const imageurl = [
+    {
+        url: "/crib.my.png",
+        Category: "Featured project",
+        Title: "Build new crib.my",
+        Content: "Build new crib website with a focus on user management and front-end design. Collaborating with other teams, we completed the project within a year and updated all dependency libraries. Our goal was to create a modern, minimalist website with a clean interface and flawless performance on all devices."
+    },
+]
 export default function Works() {
+    const [isMobile] = useMediaQuery("(max-width: 768px)");
+
     return (
-        <Box w={"1005"}>
-            <Text textAlign={"center"}>
-                Cooming soon..
-            </Text>
+        <Box mx={isMobile ? 5 : 10}>
+            <Box w="100%">
+                <Heading mb={10} textAlign={"center"}>
+                    Works 💼
+                </Heading>
+            </Box>
+            <Grid templateColumns={isMobile ? 'repeat(1, 1fr)' : 'repeat(3, 1fr)'} gap={6}>
+                {imageurl.map((d, i) => (
+                    <GridItem key={i}>
+                        <Image src={d.url} borderRadius={10} />
+                        <Box mt={5}>
+                            <Text textAlign={"center"} color={"blue.400"}>
+                                <b>{d.Category}</b>
+                            </Text>
+                            <Heading fontSize={"2xl"} textAlign={"center"}>
+                                {d.Title}
+                            </Heading>
+                            <Text fontSize={"md"} textAlign={"center"}>
+                                {d.Content}
+                            </Text>
+                        </Box>
+                    </GridItem>
+                ))}
+            </Grid>
         </Box>
     )
 }
