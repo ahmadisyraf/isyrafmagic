00002cfabf4f3b6be76bbe5a1098db9aedf0f35d
ahmadisyraf <isyrafmagic@gmail.com>
2024-08-16 00:18:22 +0800

    update: add achievement

--- screens/Home/Aboutme.js
+++ screens/Home/Aboutme.js
@@ -25,6 +25,7 @@ const AboutmeScreen = ({ isMobile, aboutMeRef }) => {
           <TabList>
             <Tab>Work</Tab>
             <Tab>Education</Tab>
+            <Tab>Achievement</Tab>
           </TabList>
 
           <TabPanels mt={isMobile ? 5 : 0}>
@@ -78,6 +79,31 @@ const AboutmeScreen = ({ isMobile, aboutMeRef }) => {
                 </Stack>
               </Box>
             </TabPanel>
+            <TabPanel px={0}>
+              <Box height={"100%"} px={isMobile ? 0 : 100}>
+                <Stack spacing={"10px"} direction={"column"}>
+                  {aboutMeData.achievement.map((achieve, index) => (
+                    <Stack key={index} spacing={2}>
+                      <Heading fontSize={"xl"}>{achieve.name}</Heading>
+                      {/* <Text fontSize={"sm"} color={"gray.600"}>
+                        {edu.university}
+                      </Text>
+                      <UnorderedList spacing={2}>
+                        <ListItem color={"gray.600"} fontSize={"md"}>
+                          {edu.cgpa}
+                        </ListItem>
+                        <ListItem color={"gray.600"} fontSize={"md"}>
+                          {edu.duration}
+                        </ListItem>
+                        <ListItem color={"gray.600"} fontSize={"md"}>
+                          {edu.address}
+                        </ListItem>
+                      </UnorderedList> */}
+                    </Stack>
+                  ))}
+                </Stack>
+              </Box>
+            </TabPanel>
           </TabPanels>
         </Tabs>
       </Stack>
@@ -88,7 +114,13 @@ const AboutmeScreen = ({ isMobile, aboutMeRef }) => {
 const aboutMeData = {
   work: [
     {
-      title: "Part-Time Job (Software Engineer) @ My Crib Sdn. Bhd.",
+      title:
+        "Technology Intern @ Deloitte - Innovation & Cloud Development Centre (ICDC)",
+      duration: "August 2024 - January 2025",
+      descriptions: ["Unknown"],
+    },
+    {
+      title: "Part-Time Software Engineer @ My Crib Sdn. Bhd.",
       duration: "January 2022 - April 2024",
       descriptions: [
         "Developed a real estate platform web app for buyers, investors, and realtors.",
@@ -97,7 +129,7 @@ const aboutMeData = {
       ],
     },
     {
-      title: "Internship Trainee (Web developer) @ My Crib Sdn. Bhd.",
+      title: "Web developer Intern @ My Crib Sdn. Bhd.",
       duration: "August 2021 - January 2022",
       descriptions: [
         "Built a real estate marketplace web app with analytical tools.",
@@ -121,6 +153,20 @@ const aboutMeData = {
       duration: "June 2019 - October 2021",
       address:
         "Universiti Malaysia Pahang Al-Sultan Abdullah, 26600, Pekan, Pahang",
+    },
+  ],
+  achievement: [
+    {
+      name: "Gold Award Undergraduate Project (Degree) - FYPro-Com Exhibition",
+    },
+    {
+      name: "Dean List Semester I Academic Session 2022/2023",
+    },
+    {
+      name: "Pengarah Terbaik Drama Komsas Peringkat Negeri Pahang",
+    },
+    {
+      name: "Naib Johan Drama Komsas Peringkat Negeri Pahang",
     },
   ],
 };
